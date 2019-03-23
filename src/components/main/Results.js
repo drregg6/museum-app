2c47544fbf06e61200d0009f3b7a430568dd3550
Dave <drregg6@gmail.com>
2019-03-23 15:36:36 -0400

    Trimmed down Results.js to a functional component

--- src/components/main/Results.js
+++ src/components/main/Results.js
@@ -1,37 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Result from './Result';
 import PropTypes from 'prop-types';
 
-class Results extends Component {
-    constructor(props) {
-        super(props);
-    }
+export default function Results(props) {
+    const count = props.count;
+    let results = props.getResults(props.activePage, props.results);
 
-    render() {
-        let count = this.props.count;
-        let results = this.props.getResults(this.props.activePage, this.props.results);
-
-        results = results.map(result => (
+    results = (count === null) ? (<h3>Please enter an artist...</h3>) : (count === 0) ? (<h3>Artist not found.</h3>) : (
+            results.map(result => (
             <Result
                 key={result.objectNumber}
                 result={result}
             />
         ))
+    )
 
-        return (
-            <div className="results">
-                {(count === null) && <h3>Please enter an artist!</h3>}
-                {(count === 0) && <h3>Artist not found...</h3>}
-                {results}
-            </div>
-        )
-    }
+    return (
+        <div className="results">
+            {results}
+        </div>
+    )
 }
-
-Results.propTypes = {
-    results: PropTypes.array.isRequired,
-    count: PropTypes.number.isRequired,
-    getResults: PropTypes.func.isRequired
-}
-
-export default Results;
